77c85c0fccfa50637788237c36306078054b8304
Ed Markovich <em1258@stern.nyu.edu>
2020-07-31 18:40:18 -0400

    card beats functionality and test

--- card.py
+++ card.py
@@ -8,7 +8,19 @@ class Card:
     def __init__(self, suit, rank):
         if suit not in Card.suits or rank not in Card.ranks:
             raise Exception ("Invalid card: " + suit + rank)
-            return
         self.suit = suit
         self.rank = rank
 
+    def beats(self, other, trump):
+        
+        if trump not in Card.suits:
+            raise Exception ("Invalid trump: " + trump)
+
+        if self.rank == other.rank and self.suit == other.suit:
+            raise Exception ("Comparing to self")
+                
+        if self.suit == other.suit:
+            return Card.ranks.index(self.rank) > Card.ranks.index(other.rank) 
+
+        return self.suit == trump
+        
--- test_card.py
+++ test_card.py
@@ -1,7 +1,7 @@
 import unittest
 from card import Card
 
-class CardTestCase(unittest.TestCase):
+class CardInitTestCase(unittest.TestCase):
 
     def test_create_valid_card(self):
         c = Card('♠', '8')
@@ -17,4 +17,44 @@ class CardTestCase(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             c = Card('♠', 'Z')
         self.assertTrue((str(context.exception)).startswith("Invalid card:"))
+
+        
+class CardBeatTestCase(unittest.TestCase):
+
+    def test_case_beat_self(self):
+        with self.assertRaises(Exception) as context:
+            c1 = Card('♠', '8')
+            c2 = Card('♠', '8')
+            c1.beats(c2, '♠')
+        self.assertTrue((str(context.exception)).startswith("Comparing to self"))
+
+    def test_case_beat_bad_trump(self):
+        with self.assertRaises(Exception) as context:
+            c1 = Card('♠', '8')
+            c2 = Card('♠', '9')
+            c1.beats(c2, 'X')
+        self.assertTrue((str(context.exception)).startswith("Invalid trump"))
+
+    def test_case_beat_trump_wins(self):
+        c1 = Card('♠', 'A')
+        c2 = Card('♦', 'K')
+        self.assertFalse(c1.beats(c2, c2.suit))
+        self.assertTrue(c1.beats(c2, c1.suit))
+
+    def test_case_beat_suits_must_match(self):
+        c1 = Card('♠', 'A')
+        c2 = Card('♦', 'K')
+        self.assertFalse(c1.beats(c2, '♣'))
+
+    def test_case_beat_big_card_wins(self):
+        c1 = Card('♠', 'A')
+        c2 = Card('♠', '10')
+        self.assertTrue(c1.beats(c2, '♣'))
+        self.assertFalse(c2.beats(c1, '♣'))
+
+    def test_case_beat_big_trump_wins(self):
+        c1 = Card('♠', 'A')
+        c2 = Card('♠', '10')
+        self.assertTrue(c1.beats(c2,'♠' ))
+        self.assertFalse(c2.beats(c1, '♠'))
         
