f3fa79b002295395cad068b98423b4a11495ffa7
Ed Markovich <em1258@stern.nyu.edu>
2020-07-31 22:39:34 -0400

    Added Game with init and add players

---
+++ game.py
@@ -0,0 +1,24 @@
+from deck import Deck
+from player import Player
+
+class Game:
+
+    def __init__(self, expect_players):
+
+        if expect_players < 2 or expect_players > 6:
+            raise Exception("Invalid expected players" + str(expect_players))
+        
+        self.expect_players = expect_players
+        self.players = {}
+        self.deck = Deck()
+        self.trump = self.deck.peek_last().suit
+
+    def add_player(self, name):
+        if name in self.players:
+            raise Exception ("Duplicate player "+ name)
+
+        if len(self.players) == self.expect_players:
+            raise Exception ("Game full")
+
+        self.players[name] = Player()
+        self.players[name].add_cards(self.deck.draw(6))
---
+++ tests/test_game.py
@@ -0,0 +1,45 @@
+import unittest
+from game import Game
+from card import Card
+
+class GameTestCase(unittest.TestCase):
+
+    def test_game_init(self):
+        game = Game(2)
+        self.assertTrue(len(game.deck.cards) == 36)
+        self.assertTrue(game.trump in Card.suits)
+
+        game = Game(6)
+        self.assertTrue(len(game.deck.cards) == 36)
+        
+        with self.assertRaises(Exception) as context:
+            game = Game(1)       
+        self.assertTrue((str(context.exception)).startswith("Invalid expected"))
+    
+        with self.assertRaises(Exception) as context:
+            game = Game(7)
+        self.assertTrue((str(context.exception)).startswith("Invalid expected"))
+        
+        
+
+    def test_game_add_player(self):
+        game = Game(4)
+        game.add_player("Ed")
+        self.assertTrue(len(game.players["Ed"].hand) == 6)
+        self.assertTrue(len(game.deck.cards) == 30)
+
+        game.add_player("Mike")
+        self.assertTrue(len(game.players["Mike"].hand) == 6)
+        self.assertTrue(len(game.deck.cards) == 24)
+
+        with self.assertRaises(Exception) as context:
+            game.add_player("Mike")
+        self.assertTrue((str(context.exception)).startswith("Duplicate player"))
+
+        game.add_player("Jon")
+        game.add_player("Conway")
+        self.assertTrue(len(game.deck.cards) == 12) 
+
+        with self.assertRaises(Exception) as context:
+            game.add_player("Fred")
+        self.assertTrue((str(context.exception)).startswith("Game full"))
