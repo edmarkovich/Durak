5e1e64e77340c74d689cafd185bba3844390e454
Ed Markovich <em1258@stern.nyu.edu>
2020-08-01 12:58:34 -0400

    Added console class and tests

---
+++ tests/test_console.py
@@ -0,0 +1,35 @@
+import unittest
+from console import Console
+
+class ConsoleTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        Console.getInstance().destroy()
+    
+    def test_console_init(self):
+        console = Console()
+        self.assertEquals(console.lines, [])
+        
+        with self.assertRaises(Exception) as context:
+            console = Console()
+        self.assertTrue((str(context.exception)).startswith("Second inst"))
+
+    def test_console_instance(self):
+        x = Console.getInstance()
+        self.assertTrue(x)
+        
+        y = Console.getInstance()        
+        self.assertEqual(x,y)
+        
+    def test_console_add(self):
+        x = Console.getInstance()
+        return
+        x.add("One")
+        
+        y = Console.getInstance()
+        y.add("Two")
+
+        self.assertEquals(len(y.lines), 2)
+        self.assertEquals(y.lines[-2], "One")
+        
+        
