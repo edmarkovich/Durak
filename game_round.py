b7b8fcc73b99f5fa735e1f04e0c1be035de803e2
Ed Markovich <em1258@stern.nyu.edu>
2020-08-01 19:34:02 -0400

    Defend logic + tests

--- game_round.py
+++ game_round.py
@@ -17,6 +17,23 @@ class GameRound:
         
         self.table.attack(card, self.players.players[self.attacker])
 
+    def defend(self):
+        move = IOUtil.get_input(source=None, player=self.defender)
+        #TODO: validate message
+
+        if move["action"] == "take":
+            self.table.take_pile(self.players.players[self.defender])
+            return "took"
+
+        card = move["card"]
+        if not self.table.defend(card, self.players.players[self.defender]):
+            return "fail"
+
+        if self.players.players[self.defender].has_cards():
+            return "beat_one"
+        
+        return "beat_all"
+
 
 
 
--- tests/test_game_round.py
+++ tests/test_game_round.py
@@ -16,16 +16,20 @@ class GameRoundTestCase(unittest.TestCase):
         self.assertEqual(game_round.defender, "B")
         self.assertTrue(game_round.table)
 
-    def test_game_round_first_attack(self):
+
+    def set_up_game_round(self):
         c1 = Card('♠', 'A')
-        c2 = Card('♦', 'K')  
+        c2 = Card('♠', 'J')  
 
         players = Players(Deck(),2)      
         players.add_player("Ed")
         players.add_player("Fred")
         players.players["Ed"].hand = [c1, c2]
         players.players["Fred"].hand = [c1, c2]
-        game_round = GameRound(players, "Ed", "Fred", '♠')
+        return GameRound(players, "Ed", "Fred", '♠')
+
+    def test_game_round_first_attack(self):
+        game_round = self.set_up_game_round()
 
         #Invalid Attack
         IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠Q"}'
@@ -33,12 +37,62 @@ class GameRoundTestCase(unittest.TestCase):
             game_round.first_attack()
         self.assertTrue((str(context.exception)).startswith("Card not in hand"))
         self.assertEqual(len(game_round.table.pile),0)
-        self.assertEqual(len(players.players["Ed"].hand),2)
+        self.assertEqual(len(game_round.players.players["Ed"].hand),2)
 
         #Valid Attack
         IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠A"}'
         game_round.first_attack()
         self.assertEqual(len(game_round.table.pile),1)
-        self.assertEqual(len(players.players["Ed"].hand),1)
+        self.assertEqual(len(game_round.players.players["Ed"].hand),1)
         
+    def test_game_round_defend_take(self):
+        game_round = self.set_up_game_round()
+        IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠A"}'
+        game_round.first_attack()
 
+        IOUtil.defaultSource = lambda: '{"action": "take"}'
+        self.assertEqual(game_round.defend(), "took")
+        self.assertEqual(len(game_round.players.players["Fred"].hand),3)
+
+    def test_game_round_defend_beat_one(self):
+        game_round = self.set_up_game_round()
+        IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠J"}'
+        game_round.first_attack()
+
+        IOUtil.defaultSource = lambda: '{"action": "beat", "card": "♠A"}'
+        self.assertEqual(game_round.defend(), "beat_one")
+        self.assertEqual(len(game_round.players.players["Fred"].hand),1)
+
+    def test_game_round_defend_beat_all(self):
+        game_round = self.set_up_game_round()        
+        IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠J"}'
+        game_round.first_attack()
+
+        IOUtil.defaultSource = lambda: '{"action": "beat", "card": "♠A"}'
+        game_round.players.players["Fred"].hand = [Card('♠', 'A')]
+
+        self.assertEqual(game_round.defend(), "beat_all")
+        self.assertEqual(len(game_round.players.players["Fred"].hand),0)
+
+    def test_game_round_defend_beat_fail(self):
+        game_round = self.set_up_game_round()        
+        IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠A"}'
+        game_round.first_attack()
+
+        IOUtil.defaultSource = lambda: '{"action": "beat", "card": "♠J"}'
+
+        self.assertEqual(game_round.defend(), "fail")
+        self.assertEqual(len(game_round.players.players["Fred"].hand),2)        
+
+
+    def test_game_round_defend_beat_bogus(self):
+        game_round = self.set_up_game_round()        
+        IOUtil.defaultSource = lambda: '{"action": "add", "card": "♠J"}'
+        game_round.first_attack()
+
+        IOUtil.defaultSource = lambda: '{"action": "beat", "card": "♠Q"}'
+
+        with self.assertRaises(Exception) as context:
+            game_round.defend()
+        self.assertTrue((str(context.exception)).startswith("Card not in hand"))
+        self.assertEqual(len(game_round.players.players["Fred"].hand),2)        
