ec89b5de13818f337ded22add37e2808cf501b3e
Ed Markovich <em1258@stern.nyu.edu>
2020-07-31 23:28:18 -0400

    Added hand_by_suits_sorted and test

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from card import Card
 
 class Player:
 
@@ -14,6 +15,14 @@ class Player:
         if card not in self.hand:
             raise Exception("Card not in hand " + str(card) + ":" + str(self))
         self.hand.remove(card)
+
+    def hand_by_suits_sorted(self):
+        out = {}
+        for suit in Card.suits:
+            temp = list(filter(lambda x: x.suit == suit, self.hand))
+            out[suit] = sorted(temp, key=lambda x: Card.ranks.index(x.rank))
+        return out
+                
 
     def __str__(self):        
         out = ""
