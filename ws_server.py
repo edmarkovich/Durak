c64f5013371906a1a0ba8c851f5e79bf5159c2d4
Ed Markovich <em1258@stern.nyu.edu>
2020-08-03 01:05:19 -0400

    basic MT WS

--- ws_server.py
+++ ws_server.py
@@ -1,41 +1,41 @@
 #https://websockets.readthedocs.io/en/stable/intro.html
 
-
 import asyncio
 import websockets
 import json
 
+import threading
+import queue
+
 from server.game import Game
 from server.ioutil import IOUtil
 
+queue = queue.Queue(1)
 
-x=0
-async def inp():
-    global x
-    while True:
-        if x==1:
-            print("YESS!")
-            x=0
-        print("Ran")
-        await asyncio.sleep(1)
-        print("Slept")
+class WSThread(threading.Thread):
+    def __init__(self):
+      threading.Thread.__init__(self)
+    def run(self):
+      async def update(websocket, path):
+        while True:
+           await websocket.send("Hello")
+           async for message in websocket:
+             data = message #= json.loads(message)
+             queue.put(data)
+             #await websocket.send("Got: "+data)            
+
+      asyncio.set_event_loop(asyncio.new_event_loop())
+      start_server = websockets.serve(update, "192.168.0.111", 5678)
+      asyncio.get_event_loop().run_until_complete(start_server)
+      asyncio.get_event_loop().run_forever()
+
+wsthread = WSThread()
+wsthread.start()
+
+game = Game(4)
+IOUtil.defaultSource = queue.get
+IOUtil.game = game
+
+game.main_loop()
 
 
-async def update(websocket, path):
-    global x
-    while True:
-        await websocket.send("Hello")
-        async for message in websocket:
-            data = message #= json.loads(message)
-            x=1
-            await websocket.send("Got: "+data)            
-
-#game = Game(4)
-#IOUtil.defaultSource = foo
-#IOUtil.game = game
-
-start_server = websockets.serve(update, "192.168.0.111", 5678)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_until_complete(inp())
-asyncio.get_event_loop().run_forever()
