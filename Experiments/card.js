da212a9e8a7e8f41c2c431609b74e475ac196fc9
Ed Markovich <em1258@stern.nyu.edu>
2020-08-08 18:44:39 -0700

    cleaned up shadows

--- Experiments/card.js
+++ Experiments/card.js
@@ -62,11 +62,9 @@ async function put_trump(trump_card) {
 
 
     mathX = "calc((1.2 * 0 * var(--card_width)) + 40px)"
-    mathY = "calc(var(--card_height) * "+2.0+ " - " + 10 + "px)"
+    mathY = "calc(var(--card_height) * "+2.0+ " + " + 10 + "px)"
     move_and_transform(node, mathX, mathY,500,"rotate3d(0,0,1,90deg)")
 
-
-    //move_and_transform(node, mathX, mathY,500,"rotate3d(0,0,1,90deg) rotate3d(0,1,0,-180deg)")
     await sleep(600)
     
     
@@ -74,12 +72,12 @@ async function put_trump(trump_card) {
     //Put the deck over it
     for (i=1; i<nodes.length; i++) {
         node = nodes[i]
-        mathX = "calc((1.2 * 0 * var(--card_width)) - " + 0.5*i+ "px + 15px)"
-        mathY = "calc(var(--card_height) * "+2.0+ " - " + 0.5*24+ "px)"
+        mathX = "calc((1.2 * 0 * var(--card_width)) - " + 1 + "px)"
+        mathY = "calc(var(--card_height) * "+2.0+ ")"
         move_and_transform(node, mathX, mathY,200,"")
         await sleep(50)
     } 
-    await sleep(1200)
+    await sleep(200)
 }
 
 function get_top_of_deck() {
@@ -94,7 +92,7 @@ async function deal_one(player_row, number_cards) {
         node = get_top_of_deck()
 
         await node.classList.remove("deck")
-        if (player_row==5) {
+        if (player_row==4) {
             flip_card(node)
         }
         mathX = "calc(1.2 * var(--card_width) * "+(i+1)+ ")"
