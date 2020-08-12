35e4680c872a6f5a2703da9591d118650e18d462
Ed Markovich <em1258@stern.nyu.edu>
2020-08-11 23:32:42 -0400

    make verb cards work fine

--- Experiments/animations.js
+++ Experiments/animations.js
@@ -220,31 +220,27 @@ async function play_own(card, mode) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function make_verb_card(verb) {
     let node = null
     let nodes = document.getElementsByClassName("verb")
     if (nodes.length==0) {
         node = document.createElement("div")
         node.setAttribute("class", "card-container verb hidden")
-    
+
         let inner = document.createElement("div")
         inner.classList.add("card-inner")
-        node.appendChild(inner);
+
+        let front = document.createElement("div");
+        front.setAttribute("class","front")
+
+        let back = document.createElement("div");
+        back.setAttribute("class","back")
+        node.appendChild(back);
+        node.appendChild(front);
+
+        front.appendChild(inner)
         document.body.appendChild(node)
+        flip_card(node)
     } else {
         node = nodes[0]
     }
@@ -253,12 +249,26 @@ function make_verb_card(verb) {
         node.classList.add("hidden")
     } else {
         node.setAttribute("onclick", "send_verb('"+verb+"')")
-        node.getElementsByClassName("card-inner")[0].innerHTML =(verb == "pass")?"done":verb
+        node.getElementsByClassName("front")[0].getElementsByClassName("card-inner")[0].innerHTML =(verb == "pass")?"done":verb
         node.classList.remove("hidden")
         animate_transform(node, 
             getTransform(animation_state.hand.length+2, 0, 4, 0), 0)
     }    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
