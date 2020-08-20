644d33354910118265e703c2070af698bc23a4c4
Ed Markovich <em1258@stern.nyu.edu>
2020-08-20 00:51:05 -0400

    progress on removing hardcoded 2 playes

--- client/table.js
+++ client/table.js
@@ -1,6 +1,6 @@
 import { Card } from './card.js';
 import { Hand, OtherHand } from "./hand.js";
-import { animate_transform} from "./utils.js"
+import { animate_transform, sleep} from "./utils.js"
 
 export class Table {
 
@@ -45,56 +45,80 @@ export class Table {
 
     async table_to_hand(player_name, player_hand) {
         let waits = []
-        while (document.getElementsByClassName("table").length>0) {
-            let node = document.getElementsByClassName("table")[0]
+        let nodes = document.getElementsByClassName("table")
+
+
+
+        for (let i=0; i<nodes.length; ++i) {
+            let node = nodes[i]
             let card = node.id;
             
             if (player_hand.indexOf(card) != -1) {
+                //nodes.push(node)
                 node.classList.remove("table")
                 await Card.make_deck_card(node)
                 waits.push(this.getOtherHand(player_name).add_card(node))
-            } 
+            }
         }
+
+        /*for (let i=0; i<nodes.length; ++i) {
+            let node = nodes[i]
+            node.classList.remove("table")
+            await Card.make_deck_card(node)
+            waits.push(this.getOtherHand(player_name).add_card(node))
+        }*/
+
         await Promise.all(waits)
-        this.last_attack_slot=-1      
     }
 
     async clear(my_hand, other_hands) {
         let waits = []
 
         for (let player in other_hands) {
+            console.log("table to hand", player)
             await this.table_to_hand(player, other_hands[player])
+            await sleep(3000)
         }
 
+        console.log("Table to either my hand or done")
         while (document.getElementsByClassName("table").length>0) {
             let node = document.getElementsByClassName("table")[0]
             let card = node.id;
             node.classList.remove("table")
 
             if (my_hand.indexOf(card) != -1) {
-                this.getHand().add_card(card,node)
+                await this.getHand().add_card(card,node)
                 waits.push(this.getHand().arrange());
             } else {
                 // Put in the done pile
-                Card.flip_card(node, true)
-                waits.push(animate_transform(node, Card.getTransform(9,0,2,0), 300))
+                await Card.flip_card(node, true)
+                //waits.push(animate_transform(node, Card.getTransform(9,0,2,0), 300))
+                await animate_transform(node, Card.getTransform(9,0,2,0), 300)
             }
         }
         await Promise.all(waits)
+        await sleep(3000)
         this.last_attack_slot=-1
     }
 
     async prepare_next_round(my_hand, other_hands) {
+        console.log("Clearing")
         await this.clear(my_hand,other_hands) 
+        await sleep(2000)
 
+        console.log("refill my hand")
         await this.getHand().refill(my_hand)
+        await sleep(2000)
 
         for (let player in other_hands) {
+            console.log("refill", player)
             await this.getOtherHand(player).refill(other_hands[player])
+            await sleep(2000)
         }
     }
 
     async play(card, player_name) {
+        console.log("Play", card, player_name)
         let hand = player_name == this.my_name ? this.getHand() : this.getOtherHand(player_name)
         let node = hand.pop_card(card);
         await this.card_to_table(node, this.mode)
@@ -102,22 +126,25 @@ export class Table {
     }
 
     async render_turn(old_table_cards, new_table_cards, old_hand, old_other_hands) {
-
         Card.make_verb_card(null)
         Table.state.theTable.getHand().glow(false)
-        //Table.state.theTable.getOtherHand().glow(false)
+
+        for (let name in old_other_hands) {
+            Table.state.theTable.getOtherHand(name).glow(false)
+        }
 
         let table_to_add = new_table_cards.filter(x => !old_table_cards.includes(x) );
         for (let i=0; i<table_to_add.length; i++) {
             
             if( old_hand.indexOf(table_to_add[i]) != -1) {
-                await this.play(table_to_add[i], true)
+                await this.play(table_to_add[i], this.my_name)
             } else { 
                 //if( old_other_hand.indexOf(table_to_add[i]) != -1) {
                 // await this.play(table_to_add[i], false)
                 for (let name in old_other_hands) {
+                    //console.log("   Name", name, old_other_hands[name], this.table_to_hand[i])
                     Table.state.theTable.getOtherHand(name).glow(false)
-                    if (old_other_hands[name].indexOf(this.table_to_hand[i]) != -1 ) {
+                    if (old_other_hands[name].indexOf(table_to_add[i]) != -1 ) {
                         await this.play(table_to_add[i], name)
                     }
                 }
